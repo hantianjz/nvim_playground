431ea01d433e94ca12151cce0c853ed38e7a7c8c
James Zhao <hjz@hackjumpzero.ca>
2024-12-17 14:36:57 -0500

    save work

--- run.lua
+++ run.lua
@@ -1,18 +1,19 @@
 print("!@34")
+
 P(vim.api.nvim_list_runtime_paths())
 
-logger = require("plenary.log").new({ plugin = "my_plugin", level = "info", use_console = "sync" })
+local logger = require("plenary.log").new({ plugin = "my_plugin", level = "info", use_console = "sync" })
 logger.info("@13")
 logger.info("@13")
 
-function ts_diff(start_ts, end_ts) 
+local function ts_diff(start_ts, end_ts)
   local sec = end_ts.sec - start_ts.sec
   local nsec = end_ts.nsec - start_ts.nsec
   if nsec < 0 then
     nsec = 1000000000 + nsec
     sec = sec - 1
   end
-  pad = string.rep("0", 9-string.len(""..nsec))
+  local pad = string.rep("0", 9-string.len(""..nsec))
 
   return sec.. "."..pad..nsec
 end
@@ -44,25 +45,44 @@ local function recursive_child_iter(node, table_to_insert, desired_types)
   end
 end
 
-local buffer = 1
-if vim.treesitter.language.add('lua') then
-  vim.treesitter.start(buffer, 'lua')
-end
 
-
-function on_change()
-print("changed tree")
-end
-local parser = vim.treesitter.get_parser( buffer)
-parser:register_cbs({on_changetree=on_change}, true)
+local start_ts = vim.uv.clock_gettime("realtime")
+local parser = vim.treesitter.get_parser(1)
 local trees = parser:parse()
-
 local root = trees[1]:root()
 
-
-local children = {}
+children = {}
 recursive_child_iter(root, children, { "identifier", "type_identifier", "field_identifier" })
 
 for _, nn in ipairs(children) do
-  local node_text = vim.treesitter.get_node_text(nn, buffer)
+  local node_text = vim.treesitter.get_node_text(nn, 1)
+  --print(node_text)
 end
+local end_ts = vim.uv.clock_gettime("realtime")
+print(ts_diff(start_ts, end_ts))
+
+local start_ts = vim.uv.clock_gettime("realtime")
+local parser = vim.treesitter.get_parser(1)
+local tree = parser:parse()[1]
+
+local query = vim.treesitter.query.parse(parser:lang(), "(identifier) @id")
+P(query)
+for id, node, metadata, match in query:iter_captures(tree:root(), 1, 76, 77) do
+  local type = node:type() -- type of the captured node
+  local row1, col1, row2, col2 = node:range() -- range of the capture
+  print(vim.treesitter.get_node_text(node, 1).."- ".. node:parent():type().." -".."; ["..row1..":"..col1.."; "..row2..":"..col2.."]")
+end
+
+local end_ts = vim.uv.clock_gettime("realtime")
+print(ts_diff(start_ts, end_ts))
+print(ts_diff(start_ts, end_ts))
+
+if next({}) then
+  print("true")
+else
+  print("false")
+end
+abecd
+
+vim.api.nvim_buf_set_extmark(1, 2, 88, 2, { end_col = 7, hl_group = "sfg_1138e5" })
+
