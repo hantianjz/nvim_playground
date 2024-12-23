42b1292035bcea817fe8ed87a5a908149e260bda
James Zhao <james@tryfi.com>
2024-12-23 17:56:21 -0500

    play

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ require("nvim_cfg")
 vim.keymap.set("n", "<Leader>x", ":.lua<CR>")
 vim.keymap.set("v", "<Leader>x", ":lua<CR>")
 vim.keymap.set("n", "<Leader>lt", ":Inspect<CR>")
+vim.keymap.set("n", '<Leader><Leader>i', [[:Inspect!<cr>]])
 
 require("config.lazy")
--- run.lua
+++ run.lua
@@ -2,7 +2,7 @@ print("!@34")
 
 P(vim.api.nvim_list_runtime_paths())
 
-local logger = require("plenary.log").new({ plugin = "my_plugin", level = "info", use_console = "sync" })
+local logger = require("plenary.log").new({ plugin = "my_plugin", level = "info"})
 logger.info("@13")
 logger.info("@13")
 
@@ -13,18 +13,18 @@ local function ts_diff(start_ts, end_ts)
     nsec = 1000000000 + nsec
     sec = sec - 1
   end
-  local pad = string.rep("0", 9-string.len(""..nsec))
+  local pad = string.rep("0", 9 - string.len("" .. nsec))
 
-  return sec.. "."..pad..nsec
+  return sec .. "." .. pad .. nsec
 end
 
-local a  = vim.uv.clock_gettime("realtime")
-local b  = vim.uv.clock_gettime("realtime")
+local a = vim.uv.clock_gettime("realtime")
+local b = vim.uv.clock_gettime("realtime")
 P(a)
 P(b)
-print("diff: ",  ts_diff(a, b))
+print("diff: ", ts_diff(a, b))
 
-P(vim.api.nvim_get_autocmds({ group="semhl", buffer=2}))
+P(vim.api.nvim_get_autocmds({ group = "semhl", buffer = 2 }))
 P(vim.bo[1].filetype)
 
 print(math.random(1))
@@ -67,10 +67,11 @@ local tree = parser:parse()[1]
 
 local query = vim.treesitter.query.parse(parser:lang(), "(identifier) @id")
 P(query)
-for id, node, metadata, match in query:iter_captures(tree:root(), 1, 76, 77) do
-  local type = node:type() -- type of the captured node
+for id, node, metadata, match in query:iter_captures(tree:root(), 1, 101, 77) do
+  local type = node:type()                    -- type of the captured node
   local row1, col1, row2, col2 = node:range() -- range of the capture
-  print(vim.treesitter.get_node_text(node, 1).."- ".. node:parent():type().." -".."; ["..row1..":"..col1.."; "..row2..":"..col2.."]")
+  print(vim.treesitter.get_node_text(node, 1) ..
+    "- " .. node:parent():type() .. " -" .. "; [" .. row1 .. ":" .. col1 .. "; " .. row2 .. ":" .. col2 .. "]")
 end
 
 local end_ts = vim.uv.clock_gettime("realtime")
@@ -84,5 +85,41 @@ else
 end
 abecd
 
-vim.api.nvim_buf_set_extmark(1, 2, 88, 2, { end_col = 7, hl_group = "sfg_1138e5" })
+for x in pairs({ 1, 2, 3 }) do
+  print(x)
+end
+vim.api.nvim_set_hl_ns(2)
 
+local existing_extmark = vim.api.nvim_buf_get_extmarks(1, 2, { 100, 0 }, { 102, -1 }, {})
+P(existing_extmark)
+for _, mark in pairs(existing_extmark) do
+  id, row, col = unpack(mark)
+  vim.api.nvim_buf_del_extmark(1, 2, id)
+end
+local extmark = vim.api.nvim_buf_get_extmark_by_id(1, 2, 243, { details = true, hl_name = true })
+P(extmark)
+
+vim.api.nvim_buf_del_extmark(1, 2, 208)
+vim.api.nvim_buf_set_extmark(1, 2, 100, 0,
+  { end_col = 5, hl_group = "sfg_1138e5", end_right_gravity = false, right_gravity = false, invalidate = true, undo_restore = true })
+
+vim.api.nvim_buf_set_extmark(1, 2, 100, 0,
+  { end_col = 5, hl_group = "sfg_1138e5", end_right_gravity = false, right_gravity = false, invalidate = true, undo_restore = true })
+vim.api.nvim_buf_set_extmark(1, 2, 100, 0,
+  { end_col = 5, hl_group = "sfg_1138e5", end_right_gravity = false, right_gravity = false, invalidate = true, undo_restore = true })
+dfalsdfkjsdlkfdffldfkdddfdf
+asdfdfdsdfalsdfkjsdlkfdffldfkdd
+asdfdfdsdfalsdfkjsdlkfdffldfkdd
+sdfdfdsdfalsdfkjsdlkfdffldfkdd
+sdfdfdsdfalsdfkjsdlkfdffldfkdd
+falsdfkjsdlkfdffldfkdddfdf
+rint(x)
+vim.ap
+alsdfjdslkjfthis
+
+local function acallback(cb)
+  print("hello async")
+end
+vim.defer_fn(acallback, 10)
+local logger = require("plenary.log").new({ plugin = "my_plugin", level = "info", use_console = "sync" })
+local logger = require("plenary.log").new({ plugin = "my_plugin", level = "info", use_console = "sync" })
